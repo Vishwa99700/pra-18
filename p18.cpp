9f7ae418deefda23a471af57ea1d53c4f832e115
Vishwa Sudaraka <vishwa25flash@gmail.com>
2024-11-17 20:24:21 +0530

    Add files via upload

---
+++ p18.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main ()
+{
+    cout<< "Enter the mark" <<endl;
+    double x;
+    cin>>x;
+    if (x>=40)
+        cout << "PASS";
+    else
+        cout << "FAIL";
+    return 0;
+}
